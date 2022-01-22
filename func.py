b06bcb4059064d63d0bdd2010040651bc55e6d1e
Medkaba <diakamadi@gmail.com>
2022-01-22 20:06:04 +0000

    pg func.py

---
+++ func.py
@@ -0,0 +1,12 @@
+# Definitions funcs
+def say_hi(dev):
+  print('Salut tout le monde: \n')
+  print('Tag :', dev)
+
+def sum(a,b):
+  return a + b
+
+print('Bienvenu dans mon programme python')
+# Appels funcs
+say_hi('KABA Mohamed')
+print('La somme de 2 + 18 =',sum(2,18))
